9d5aead1f5bdd27676341083bd9f75b5df9ceaa4
Kaivan Kamali <kxk302@psu.edu>
2023-08-28 15:28:28 -0400

    Script to calculate the max number f reads, for each chromosome in a fasta file, then subtract the number of reads from the max value, so we can use MACS2 for valley detection.

---
+++ scripts/add_valley_column.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import pathlib
+
+import pandas as pd
+
+
+def add_valley_column(input_folder, output_folder):
+  files = os.listdir(input_folder)
+  for file in files:
+    file_path = os.path.join(input_folder, file)
+    file_name = pathlib.Path(file_path).stem
+    
+    # Ignore any directories. We only process files in input_folder
+    if not os.path.isfile(file_path):
+      continue
+    print(f"{file_path} is a file")
+
+    df = pd.read_csv(file_path, sep="\t", names=["chromosome", "start", "stop", "number_of_reads"])
+    print(df.head(2))
+
+    # df["max_number_of_reads"] = df.groupby(["chromosome"])["number_of_reads"].max()
+    df_max_number_of_read = pd.DataFrame(df.groupby(["chromosome"])["number_of_reads"].max())
+    df_max_number_of_read.rename(columns={'number_of_reads': 'max_number_of_reads'}, inplace=True)
+    
+    print(df_max_number_of_read.head(20))
+
+    df_merged = pd.merge(df, df_max_number_of_read, on="chromosome", how="inner")
+    df_merged["valley_number_of_reads"] = df_merged["max_number_of_reads"] - df_merged["number_of_reads"]
+    print(df_merged.head(20))
+
+    df_merged.to_csv(os.path.join(output_folder, file_name + "_valley.tsv"), sep="\t",
+                     columns=["chromosome", "start", "stop", "valley_number_of_reads"],
+                     header=False, index=False)
+
+
+if __name__ == "__main__":
+  argParse = argparse.ArgumentParser("For each chromosome, deduct the number of reads from the maximum value of\
+                                      the number of reads, so we can find valleys by peak calling")
+
+  argParse.add_argument("-i", "--input_folder", type=str, required=True)
+  argParse.add_argument("-o", "--output_folder", type=str, required=True)
+
+  args = argParse.parse_args()
+
+  add_valley_column(args.input_folder, args.output_folder)
