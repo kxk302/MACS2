e6273eacc15679f26e40b0492ae53a9e167614c3
Kaivan Kamali <kxk302@psu.edu>
2023-10-16 13:10:59 -0400

    Added scripts that create bar plot of sample non-B DNA intersect ratios. Added requirements file

---
+++ scripts/plot_intersect_ratio.py
@@ -0,0 +1,62 @@
+import argparse
+import pandas as pd
+from matplotlib import pyplot as plt
+
+import numpy as np
+
+
+def plot_intersect_ratio(intersect_ratio_file,
+                         random_intersect_ratio_file,
+                         intersect_ratio_filter,
+                         output_file):
+  intersect_df = pd.read_csv(intersect_ratio_file, sep="\t")
+  intersect_df = intersect_df[ intersect_df.Harmonic_Mean >= intersect_ratio_filter]
+
+  random_df = pd.read_csv(random_intersect_ratio_file, sep="\t")
+
+  all_df = pd.merge(intersect_df, random_df, how="inner", on=["Window_Size"])
+  all_df.sort_values(by=["Harmonic_Mean_x"], ascending=False, inplace=True)
+  all_df.drop(columns=["Sample_Name_y"], inplace=True)
+  all_df.rename(columns={"Sample_Name_x":"Sample_Name",
+                         "Harmonic_Mean_x":"Harmonic_Mean",
+                         "Harmonic_Mean_y":"Harmonic_Mean_Random"}, inplace=True)
+  all_df["New_Sample_Name"] = all_df["Sample_Name"] + " (win " + all_df["Window_Size"].astype(str) + ")"
+  all_df.drop(columns=["Sample_Name", "Window_Size"], inplace=True)
+  all_df.rename(columns={"New_Sample_Name":"Sample_Name"}, inplace=True)
+
+  barWidth=0.1
+  fig = plt.subplots(figsize =(15, 10))
+
+  harmonic_mean = all_df["Harmonic_Mean"].tolist()
+  harmonic_mean_radom = all_df["Harmonic_Mean_Random"].tolist()
+
+  x1 = np.arange(all_df.shape[0])
+  x2 = [x + barWidth for x in x1]
+
+  plt.bar(x1, harmonic_mean, label="Sample")
+  plt.bar(x2, harmonic_mean_radom, label="Random")
+
+  plt.xlabel('Sample Name (Window Size)', fontweight ="bold", fontsize = 15)
+  plt.ylabel('Non-B DNA Intersect Ratio', fontweight ="bold", fontsize = 15)
+  plt.xticks(x1, all_df["Sample_Name"].tolist(), rotation=75, fontweight="bold", fontsize=7)
+
+  plt.subplots_adjust(bottom=0.3)
+  plt.legend()
+  plt.savefig(output_file)
+
+
+if __name__ == "__main__":
+
+  argParse = argparse.ArgumentParser("Bar plot for non-B DNA intersect ratio of \
+                                      samples and non-B DNA intersct ratio of random \
+                                      intervals with the same window size")
+  argParse.add_argument("-i", "--intersect_ratio_file", type=str, required=True)
+  argParse.add_argument("-r", "--random_intersect_ratio_file", type=str, required=True)
+  argParse.add_argument("-f", "--intersect_ratio_filter", type=float, required=True)
+  argParse.add_argument("-o", "--output_file", type=str, required=True)
+
+  args = argParse.parse_args()
+  plot_intersect_ratio(args.intersect_ratio_file,
+                       args.random_intersect_ratio_file,
+                       args.intersect_ratio_filter,
+                       args.output_file)
---
+++ scripts/plot_intersect_ratio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ./scripts/plot_intersect_ratio.py -i ./output/intersect_summaries.tsv -r ./output/random/random_intersect_summaries.tsv -f 0.45 -o ./output/intersect_ratio.png
